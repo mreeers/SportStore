9c2839e4da83ce46fcd5029542e8f0eb419beaa9
Александр Ефанов <verpl01@yandex.ru>
2020-11-19 03:40:15 +0500

    Добавлена пагинация

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportStore.Models.Interface;
+using SportStore.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,26 @@ namespace SportStore.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository repository;
+        public int pageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult List() => View(repository.Products);
-        
+        public ViewResult List(int productPage = 1) =>
+            View(new ProductListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((productPage - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
