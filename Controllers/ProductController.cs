922c64c7c230eec3762a8cf0737b2d4ba3f5cde7
Александр Ефанов <verpl01@yandex.ru>
2020-11-19 00:52:24 +0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repo = repository;
+        }
+
+        public IActionResult List() => View(repository.Products);
+        
+    }
+}
